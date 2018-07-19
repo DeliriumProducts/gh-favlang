9a010fdea19f46c9d573c03f5e08f5e0a5fe88f1
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-07-20 01:25:23 +0300

    Add request method, Add animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,52 @@
 $(document).ready(() => {
-    $('#laino').hide();
+    // INSTANCES OF DOM ELEMENTS
+    const profileDiv = $('#profile');
+    const avatarImg = $('#avatar');
+    const userInput = $('#user');
+    const userP = $('#username');
 
-    $('#search').click(() => {
-        // anime({
-        //     targets: ".st1",
-        //     fill: '#f1e05a',
-        //     easing: 'easeOutQuart'
-        // });
+    // HIDING PROFILE DIV   
+    profileDiv.hide();
+
+    // EVENTS
+    $('#search').click(async () => {
+        const data = await request(userInput.val());
+        avatarImg.attr('src', data.avatar_url);
+
+        // PRIMARY
+        anime({
+            targets: ".lang-color-primary",
+            fill: '#701516',
+            color: '#701516',
+            easing: 'easeOutQuart'
+        });
+        anime({
+            targets: "body",
+            backgroundColor: '#701516',
+            easing: 'easeOutQuart'
+        });
+
+        // SECONDARY
+        anime({
+            targets: ".btn",
+            backgroundColor: '#601415',
+            boxShadow: '0 0 10px #601415', 
+            easing: 'easeOutQuart'
+        });
+        anime({
+            targets: ".lang-color-secondary",
+            fill: '#601415',
+            color: '#601415',
+            easing: 'easeOutQuart'
+        });
+
+        profileDiv.slideDown(250);
+        userP.text(data.name);
     })
 });
+
+const request = async (user) => {
+    const response = await fetch(`https://api.github.com/users/${user}`);
+    const data = await response.json();
+    return data;
+};
