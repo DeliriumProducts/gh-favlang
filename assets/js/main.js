9b93f0024f9f125c38b982c67540c3775b86bb5b
Simo Aleksandrov <simo3003@me.com>
2018-07-20 15:46:00 +0300

    Move chroma.js reference, Add check for duplicate request

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-
+let oldUser = "";
 $(document).ready(() => {
 
     // INSTANCES OF DOM ELEMENTS
@@ -12,49 +12,52 @@ $(document).ready(() => {
 
     // EVENTS
     $('#search').click(async () => {
-        profileDiv.slideUp(500);
         const user = userInput.val();
-        let languages = [];
-        const colors = window.githubColors;
-        let primaryColor = '#425568';
-        let secondaryColor = '#334251';
+        if (user !== oldUser) {
+            profileDiv.slideUp(500);
+            let languages = [];
+            const colors = window.githubColors;
+            let primaryColor = '#425568';
+            let secondaryColor = '#334251';
 
-        const userData = await requestUser(user);
-        avatarImg.attr('src', userData.avatar_url);
-        const reposData = await requestRepos(user);
+            const userData = await requestUser(user);
+            avatarImg.attr('src', userData.avatar_url);
+            const reposData = await requestRepos(user);
 
-        // GETTING THE TOP MOST USED LANGUAGE IN EACH REPO
-        for (const repo of reposData) {
-            if (repo.fork == false) {
-                let language = repo.language;
-                if (language === null) {
-                    continue;
-                }
-                let index = findKeyInArray(languages, language);
-                if (index != undefined) {
-                    languages[index][language]++;
-                } else {
-                    languages.push({ [language]: 1 });
+            // GETTING THE TOP MOST USED LANGUAGE IN EACH REPO
+            for (const repo of reposData) {
+                if (repo.fork == false) {
+                    let language = repo.language;
+                    if (language === null) {
+                        continue;
+                    }
+                    let index = findKeyInArray(languages, language);
+                    if (index != undefined) {
+                        languages[index][language]++;
+                    } else {
+                        languages.push({ [language]: 1 });
+                    }
                 }
             }
+
+            // SORTING THE LANG ARRAY BY DESCENDING
+            languages = languages.sort((a, b) => {
+                let first = Object.values(a);
+                let second = Object.values(b);
+                return second - first;
+            });
+
+            let topLangugage = Object.keys(languages[0]);
+
+            if (colors[topLangugage].color != undefined || colors[topLangugage.color != null]) {
+                primaryColor = colors[topLangugage].color;
+                secondaryColor = chroma(primaryColor).darken(0.25).hex();
+            }
+
+            userP.text(userData.name ? userData.name : userData.login); // check if user has available public profile name and display it otherwise display username
+            animations(profileDiv, primaryColor, secondaryColor);
+            oldUser = user;
         }
-
-        // SORTING THE LANG ARRAY BY DESCENDING
-        languages = languages.sort((a, b) => {
-            let first = Object.values(a);
-            let second = Object.values(b);
-            return second - first;
-        });
-
-        let topLangugage = Object.keys(languages[0]);
-
-        if (colors[topLangugage].color != undefined || colors[topLangugage.color != null]) {
-            primaryColor = colors[topLangugage].color;
-            secondaryColor = chroma(primaryColor).darken().saturate(2).hex();
-        }
-
-        userP.text(userData.name ? userData.name : userData.login); // check if user has available public profile name and display it otherwise display username
-        animations(profileDiv, primaryColor, secondaryColor);
     })
 });
 
