634e5e36f910eba579caa2efe9b981c3b46e2670
Simo Aleksandrov <simo3003@me.com>
2018-07-21 01:44:24 +0300

    Make some variables global scoped

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,9 @@
+const colors = window.githubColors;
+
 let oldUser = "";
+let primaryColor;
+let secondaryColor;
+let languages = [];
 
 $(document).ready(() => {
 
@@ -18,10 +23,11 @@ $(document).ready(() => {
         const user = userInput.val();
         if (user !== oldUser) {
             profileDiv.slideUp(250, 'swing', async () => {
-                let languages = [];
-                const colors = window.githubColors;
-                let primaryColor = '#425568';
-                let secondaryColor = '#334251';
+                // CLEAR THE ARRAY EVERYTIME
+                languages = [];
+
+                primaryColor = '#425568';
+                secondaryColor = '#334251';
 
                 const userData = await requestUser(user);
                 if (!isNaN(userData)) {
@@ -31,55 +37,64 @@ $(document).ready(() => {
                     animations(profileDiv, '#F46C6C', '#DE6060');
                     return;
                 }
+
                 avatarImg.attr('src', userData.avatar_url);
-                const reposData = await requestRepos(user);
+                let topLangugage = await getUserTopLang(user);
 
-                // GETTING THE TOP MOST USED LANGUAGE IN EACH REPO
-                for (const repo of reposData) {
-                    if (repo.fork == false) {
-                        let language = repo.language;
-                        if (language === null) {
-                            continue;
-                        }
-                        let index = languages.findKey(language);
-                        if (index != undefined) {
-                            languages[index][language]++;
-                        } else {
-                            languages.push({ [language]: 1 });
-                        }
-                    }
-                }
-
-                // SORTING THE LANG ARRAY BY DESCENDING
-                languages = languages.sort((a, b) => {
-                    let first = Object.values(a);
-                    let second = Object.values(b);
-                    return second - first;
-                });
-                console.log(languages);
-                let topLangugage = Object.keys(languages[0]);
                 if (colors[topLangugage].color != undefined || colors[topLangugage.color != null]) {
                     primaryColor = colors[topLangugage].color;
                     secondaryColor = chroma(primaryColor).darken(0.25).hex();
                 }
-                let name = userData.name ? userData.name : userData.login; // check if user has available public profile name and display it otherwise display username
+
+                let name = userData.name ? userData.name : userData.login; // if available, display the user's real name, rather than the username
                 userP.text(name);
                 avatarA.attr('href', `https://github.com/${user}`);
                 favLangP.html(`<a href="https://github.com/${user}">${user}</a>'s favorite coding language is: <span id="language-span">${topLangugage}</span>`);
-                animations(profileDiv, primaryColor, secondaryColor);
+                animations(profileDiv);
                 oldUser = user;
             });
         }
     })
 });
 
+const getUserTopLang = async (user) => {
+
+    // REQUEST THE REPOS OF THE GIVEN USER
+    const reposData = await requestRepos(user);
+
+    // GETTING THE TOP MOST USED LANGUAGE IN EACH REPO
+    for (const repo of reposData) {
+        if (repo.fork == false) {
+            let language = repo.language;
+            if (language === null) {
+                continue;
+            }
+            let index = languages.findKey(language);
+            if (index != undefined) {
+                languages[index][language]++;
+            } else {
+                languages.push({ [language]: 1 });
+            }
+        }
+    }
+
+    // SORTING THE LANG ARRAY BY DESCENDING
+    languages = languages.sort((a, b) => {
+        let first = Object.values(a);
+        let second = Object.values(b);
+        return second - first;
+    });
+
+    // FIRST ELEMENT IS THE MOST USED LANGUAGE
+    return topLangugage = Object.keys(languages[0]);
+}
+
 const requestUser = async (user) => {
     const response = await fetch(`https://api.github.com/users/${user}`);
     if (response.status != 200) {
         return response.status;
     }
-    const data = await response.json();
-    return data;
+    return response.json();
 };
 
 const requestRepos = async (user) => {
@@ -87,11 +102,10 @@ const requestRepos = async (user) => {
     if (response.status != 200) {
         return response.status;
     }
-    const data = await response.json();
-    return data;
+    return response.json();
 };
 
-const animations = (profileDiv, primaryColor, secondaryColor) => {
+const animations = (profileDiv) => {
     // PRIMARY
     anime({
         targets: ".lang-color-primary",
@@ -123,7 +137,7 @@ const animations = (profileDiv, primaryColor, secondaryColor) => {
     profileDiv.slideDown(250);
 };
 
-Array.prototype.findKey = function(key) {
+Array.prototype.findKey = function (key) {
     for (let i = 0; i < this.length; i++) {
         if (this[i].hasOwnProperty(key)) {
             return i;
