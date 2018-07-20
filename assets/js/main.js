7df56094c3533d46852209135065a42adb78f62c
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-07-20 03:18:49 +0300

    Add fetch repos function, Add language sorter

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(() => {
+
     // INSTANCES OF DOM ELEMENTS
     const profileDiv = $('#profile');
     const avatarImg = $('#avatar');
@@ -10,43 +11,91 @@ $(document).ready(() => {
 
     // EVENTS
     $('#search').click(async () => {
-        const data = await request(userInput.val());
-        avatarImg.attr('src', data.avatar_url);
+        const user = userInput.val();
+        let languages = [];
 
-        // PRIMARY
-        anime({
-            targets: ".lang-color-primary",
-            fill: '#701516',
-            color: '#701516',
-            easing: 'easeOutQuart'
-        });
-        anime({
-            targets: "body",
-            backgroundColor: '#701516',
-            easing: 'easeOutQuart'
+        const userData = await requestUser(user);
+        avatarImg.attr('src', userData.avatar_url);
+        const reposData = await requestRepos(user);
+
+        console.log(reposData);
+        for (const repo of reposData) {
+            if (repo.fork == false) {
+                let language = repo.language;
+                if (language === null) {
+                    continue;
+                }
+                let index = findKeyInArray(languages, language);
+                if (index != undefined) {
+                    languages[index][language]++;
+                } else {
+                    languages.push({ [language]: 1 });
+                }
+            }
+        }
+
+        languages = languages.sort((a, b) => {
+            let first = Object.values(a);
+            let second = Object.values(b);
+            console.log(first, second);
+            return second - first;
         });
 
-        // SECONDARY
-        anime({
-            targets: ".btn",
-            backgroundColor: '#601415',
-            boxShadow: '0 0 10px #601415', 
-            easing: 'easeOutQuart'
-        });
-        anime({
-            targets: ".lang-color-secondary",
-            fill: '#601415',
-            color: '#601415',
-            easing: 'easeOutQuart'
-        });
-
-        profileDiv.slideDown(250);
-        userP.text(data.name);
+        console.log(languages);
+        userP.text(userData.name ? userData.name : userData.login);
+        animations(profileDiv);
     })
 });
 
-const request = async (user) => {
+const requestUser = async (user) => {
     const response = await fetch(`https://api.github.com/users/${user}`);
     const data = await response.json();
     return data;
+};
+
+const requestRepos = async (user) => {
+    const response = await fetch(`https://api.github.com/users/${user}/repos?type=all`);
+    const data = await response.json();
+    return data;
+};
+
+const animations = (profileDiv) => {
+    // PRIMARY
+    anime({
+        targets: ".lang-color-primary",
+        fill: '#701516',
+        color: '#701516',
+        easing: 'easeOutQuart'
+    });
+    anime({
+        targets: "body",
+        backgroundColor: '#701516',
+        easing: 'easeOutQuart'
+    });
+
+    // SECONDARY
+    anime({
+        targets: ".btn",
+        backgroundColor: '#601415',
+        boxShadow: '0 0 10px #601415',
+        easing: 'easeOutQuart'
+    });
+    anime({
+        targets: ".lang-color-secondary",
+        fill: '#601415',
+        color: '#601415',
+        easing: 'easeOutQuart'
+    });
+
+    // DIV SLIDE DOWN
+    profileDiv.slideDown(250);
+};
+
+const findKeyInArray = (arr, key) => {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].hasOwnProperty(key)) {
+            return i;
+        }
+    }
+    return undefined;
 };
