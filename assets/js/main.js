97eaf14ee177ed8bb0af40bce8a453c79964654a
Simo Aleksandrov <simo3003@me.com>
2018-07-21 16:15:00 +0300

    General code optimization

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,17 +29,21 @@ $(document).ready(() => {
                 primaryColor = '#425568';
                 secondaryColor = '#334251';
 
-                const userData = await requestUser(user);
-                if (!isNaN(userData)) {
-                    let errorCode = userData;
+                const { userData, topLangugage, status } = await getUserTopLang(user);
+
+                // IF THERE IS A STATUS CODE DIFFERENT THAN 200, IT'S AN ERROR
+                if (status != 200) {
+                    let errorCode = status;
                     userP.text(`Error code ${errorCode}`);
+                    favLangP.html(`Try again :c`);
                     avatarImg.attr('src', `https://http.cat/${errorCode}.jpg`);
                     animations(profileDiv, '#F46C6C', '#DE6060');
+                    oldUser = user;
                     return;
                 }
 
-                avatarImg.attr('src', userData.avatar_url);
-                let topLangugage = await getUserTopLang(user);
+                const avatarUrl = userData.avatar_url;
+                avatarImg.attr('src', avatarUrl);
 
                 if (colors[topLangugage].color != undefined || colors[topLangugage.color != null]) {
                     primaryColor = colors[topLangugage].color;
@@ -57,53 +61,6 @@ $(document).ready(() => {
     })
 });
 
-const getUserTopLang = async (user) => {
-
-    // REQUEST THE REPOS OF THE GIVEN USER
-    const reposData = await requestRepos(user);
-
-    // GETTING THE TOP MOST USED LANGUAGE IN EACH REPO
-    for (const repo of reposData) {
-        if (repo.fork == false) {
-            let language = repo.language;
-            if (language === null) {
-                continue;
-            }
-            let index = languages.findKey(language);
-            if (index != undefined) {
-                languages[index][language]++;
-            } else {
-                languages.push({ [language]: 1 });
-            }
-        }
-    }
-
-    // SORTING THE LANG ARRAY BY DESCENDING
-    languages = languages.sort((a, b) => {
-        let first = Object.values(a);
-        let second = Object.values(b);
-        return second - first;
-    });
-
-    // FIRST ELEMENT IS THE MOST USED LANGUAGE
-    return topLangugage = Object.keys(languages[0]);
-}
-
-const requestUser = async (user) => {
-    const response = await fetch(`https://api.github.com/users/${user}`);
-    if (response.status != 200) {
-        return response.status;
-    }
-    return response.json();
-};
-
-const requestRepos = async (user) => {
-    const response = await fetch(`https://api.github.com/users/${user}/repos?type=all`);
-    if (response.status != 200) {
-        return response.status;
-    }
-    return response.json();
-};
 
 const animations = (profileDiv) => {
     // PRIMARY
@@ -136,6 +93,71 @@ const animations = (profileDiv) => {
     // DIV SLIDE DOWN
     profileDiv.slideDown(250);
 };
+
+const getUserTopLang = async (user) => {
+
+    // REQUEST THE INFO FOR THE GIVEN USER
+    const userData = await requestUser(user);
+
+    // REQUEST THE REPOS FOR THE GIVEN USER
+    const reposData = await requestRepos(user);
+    // RETURN ERROR CODE IF THERE IS AN ERROR
+    if (!isNaN(userData)) {
+        return { status: userData };
+    } else if (!isNaN(reposData)) {
+        return { status: reposData };
+    }
+
+    // GETTING ALL USED LANGUAGES AMONG THE REPOS
+    for (const repo of reposData) {
+        if (repo.fork == false) {
+            let language = repo.language;
+            if (language === null) {
+                continue;
+            }
+            let index = languages.findKey(language);
+            if (index != undefined) {
+                languages[index][language]++;
+            } else {
+                languages.push({ [language]: 1 });
+            }
+        }
+    }
+
+    // SORTING THE LANGUAGE ARRAY BY DESCENDING ORDER
+    languages = languages.sort((a, b) => {
+        let first = Object.values(a);
+        let second = Object.values(b);
+        return second - first;
+    });
+
+    // FIRST ELEMENT IS THE MOST USED LANGUAGE
+    let topLangugage = Object.keys(languages[0]);
+
+    // RETURN AN OBJECT WITH ALL OF THE DATA
+    return {
+        userData, topLangugage, status: 200
+    }
+}
+
+const requestUser = async (user) => {
+    const response = await fetch(`https://api.github.com/users/${user}`);
+    // IF THE STATUS CODE IS NOT 200, RETURN IT
+    if (response.status != 200) {
+        return response.status;
+    }
+    return response.json();
+};
+
+const requestRepos = async (user) => {
+    const response = await fetch(`https://api.github.com/users/${user}/repos?type=all`);
+    // IF THE STATUS CODE IS NOT 200, RETURN IT
+    if (response.status != 200) {
+        return response.status;
+    }
+    return response.json();
+};
+
 
 Array.prototype.findKey = function (key) {
     for (let i = 0; i < this.length; i++) {
